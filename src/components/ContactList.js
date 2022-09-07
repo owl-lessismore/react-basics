96667d02b8ddbf20122988506ceef90442b4e564
Darwin <serocefinodarwin@gmail.com>
2022-09-07 18:08:53 +0800

    pass props and show props

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const ContactList = (contacts) => {
-  console.log(contacts);
-  return <div>Contact List</div>;
+const ContactList = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      Contact List
+      {props.contacts.map((contact) => {
+        return (
+          <div id={contact.id}>
+            <div>
+              <h2>{contact.name}</h2>
+              <p>{contact.email}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ContactList;
