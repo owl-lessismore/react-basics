d04b464939c4d160f91f39a6584caa2a52c93b52
Darwin <serocefinodarwin@gmail.com>
2022-09-08 22:29:10 +0800

    post contact db json server

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const LOCAL_STORAGE_KEY = 'contacts';
+  // const LOCAL_STORAGE_KEY = 'contacts';
   const BASE_URL = 'http://localhost:3006/';
 
-  const addContactHandler = (contact) => {
+  const addContactHandler = async (contact) => {
     // spread operator
-    const newContactList = [...contacts, { id: uuid(), ...contact }];
-    setContacts(newContactList);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newContactList));
+
+    const request = {
+      id: uuid(),
+      ...contact,
+    };
+
+    const response = await fetch(`${BASE_URL}contacts`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    });
+
+    const newContacts = await response.json();
+
+    setContacts([...contacts, newContacts]);
   };
 
   const removeContactHandler = (id) => {
     const newContactList = contacts.filter((contact) => contact.id !== id);
     setContacts(newContactList);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newContactList));
   };
 
   useEffect(() => {
-    // const retrieveContacts = JSON.parse(
-    //   localStorage.getItem(LOCAL_STORAGE_KEY)
-    // );
-
-    // if (retrieveContacts) {
-    //   setContacts(retrieveContacts);
-    // }
     const getAllContacts = async () => {
       const response = await fetch(`${BASE_URL}contacts`);
       const contacts = await response.json();
